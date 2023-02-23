cffcf538ca8e6194ef7a0c3c309f876a1cedeb35
kube-csc <kube@kube-csc.de>
2023-02-23 20:27:14 +0100

    User Datenbank angepasst auf das Projket Vereinsverwaltung

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             //'email' => $this->faker->unique()->safeEmail(),
             'email' => 'info@info.de',
-            'role' => 'admin',
+            'role' => '1',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'two_factor_secret' => null,
